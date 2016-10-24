df0f49411cc34cf38b8cf4a044a36d8094074fae
marcia <marcia.millanao@gmail.com>
2016-10-24 13:44:58 -0300

    trabajo traduccion formulario

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function cambiaTexto(){
+	document.getElementById("form-signin-heading").innerHTML = "Por favor inicia sesión";
+	document.getElementById("inputEmail").placeholder = "Correo Electronico";
+	document.getElementById("inputPassword").placeholder = "Contraseña";
+	document.getElementsByTagName("span")[0].innerHTML = "Recordar datos";
+	document.getElementsByClassName("btn btn-lg btn-primary btn-block submit-btn")[0].innerHTML = "Iniciar Sesion";	
+}
